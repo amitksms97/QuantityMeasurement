585fa5463735f2568fe93b42965ede74b586aee6
amtksms97 <amtksms97@gmail.com>
2021-01-24 21:20:24 +0530

    Added code to add different volume and its computation

--- enum_class.py
+++ enum_class.py
@@ -6,6 +6,20 @@ class Length(enum.Enum):
     YARD = 36.0
     INCH = 1.0
     CM = 0.4
+    GALLON = 3.78
+    LITRE = 1
+    ML = 0.001
 
     def convert(self, length):
         return self.unit * length
+
+'''''
+class Volume(enum.Enum):
+    GALLON = 3.78
+    LITRE = 1
+    ML = 0.001
+
+    def convert(self, volume):
+        return self.unit * volume
+'''''
+
