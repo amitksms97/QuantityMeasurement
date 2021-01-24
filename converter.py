169a4dbdad4f37c366a329fb9074a9517b5d718d
amtksms97 <amtksms97@gmail.com>
2021-01-24 21:08:03 +0530

    Added code to add different lengths.

--- converter.py
+++ converter.py
@@ -5,7 +5,18 @@ class QuantityMeasurement:
     def __init__(self, unit, length):
         self.unit = unit
         self.length = length
+    '''''
+    def __eq__(self, other):
+        if isinstance(other, QuantityMeasurement):
+            return self.unit == other.unit and other.length == self.length
 
+    def compareto(self, other):
+        if isinstance(self.unit, Length) and isinstance(other.unit, Length):
+            return Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length)
+
+        return False
+
+    '''''
     def __eq__(self, other):
         if isinstance(other, QuantityMeasurement):
             return self.unit == other.unit and other.length == self.length
@@ -13,3 +24,6 @@ class QuantityMeasurement:
             if Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length):
                 return True
         return False
+
+    def addition(self, other):
+        return Length.convert(self.unit, self.length) + Length.convert(other.unit, other.length)
